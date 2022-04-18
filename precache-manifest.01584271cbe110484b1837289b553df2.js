665dff7f50fe9a2da676ef620b28d928ee519643
wilhelmmichto <michto15897@gmail.com>
2022-04-18 20:16:05 +0530

    Updates

---
+++ precache-manifest.01584271cbe110484b1837289b553df2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8556d4fa7842550237c6",
+    "url": "/static/css/main.2ef7859a.chunk.css"
+  },
+  {
+    "revision": "8556d4fa7842550237c6",
+    "url": "/static/js/main.8556d4fa.chunk.js"
+  },
+  {
+    "revision": "0d61cf9e78a3bde97ce2",
+    "url": "/static/css/1.bf822aec.chunk.css"
+  },
+  {
+    "revision": "0d61cf9e78a3bde97ce2",
+    "url": "/static/js/1.0d61cf9e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "d7b8a01b77e1b45e32318b2b45fca54e",
+    "url": "/index.html"
+  }
+];
